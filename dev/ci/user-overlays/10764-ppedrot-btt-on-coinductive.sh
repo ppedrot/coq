13dc3ce4799dd52846d8565062f58d69b10fc852
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2019-09-17 18:15:23 +0200

    Adding overlays.

---
+++ dev/ci/user-overlays/10764-ppedrot-btt-on-coinductive.sh
@@ -0,0 +1,24 @@
+if [ "$CI_PULL_REQUEST" = "10764" ] || [ "$CI_BRANCH" = "btt-on-coinductive" ]; then
+
+    compcert_CI_REF=btt-on-coinductive
+    compcert_CI_GITURL=https://github.com/ppedrot/CompCert
+
+    Corn_CI_REF=btt-on-coinductive
+    Corn_CI_GITURL=https://github.com/ppedrot/corn
+
+    HoTT_CI_REF=btt-on-coinductive
+    HoTT_CI_GITURL=https://github.com/ppedrot/HoTT
+
+    inf_seq_ext_CI_REF=btt-on-coinductive
+    inf_seq_ext_CI_GITURL=https://github.com/ppedrot/InfSeqExt
+
+    quickchick_CI_REF=btt-on-coinductive
+    quickchick_CI_GITURL=https://github.com/ppedrot/QuickChick
+
+    verdi_CI_REF=btt-on-coinductive
+    verdi_CI_GITURL=https://github.com/ppedrot/verdi
+
+    vst_CI_REF=btt-on-coinductive
+    vst_CI_GITURL=https://github.com/ppedrot/VST
+
+fi
